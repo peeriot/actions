a420b2a29537a21b00bd0ca9084851396641b968
Erik Junghanns <erik.junghanns@peeriot.io>
2025-01-28 10:57:19 +0100

    Use random name for upload directory in `deploy-dep-package` action

--- deploy-dep-package/exec.sh
+++ deploy-dep-package/exec.sh
@@ -3,20 +3,22 @@
 set -e
 
 SCRIPT_DIR="$(dirname $0)"
+INCOMING_DIR="$DEPLOY_DEB_PACKAGE_DIR/incoming-$(printf '%05d%05d' $RANDOM $RANDOM)"
 
-ssh -o BatchMode=yes "$DEPLOY_DEB_HOST" "rm -rf $DEPLOY_DEB_PACKAGE_DIR/incoming && mkdir -p $DEPLOY_DEB_PACKAGE_DIR/incoming"
+ssh -o BatchMode=yes "$DEPLOY_DEB_HOST" "rm -rf \"$INCOMING_DIR\" && mkdir -p \"$INCOMING_DIR\""
+
 
 for PACKAGE in $DEPLOY_DEB_PACKAGES; do
-    scp -o BatchMode=yes "$PACKAGE" "$DEPLOY_DEB_HOST:$DEPLOY_DEB_PACKAGE_DIR/incoming/"
+    scp -o BatchMode=yes "$PACKAGE" "$DEPLOY_DEB_HOST:$INCOMING_DIR"
 done
 
 ssh -o BatchMode=yes \
     "$DEPLOY_DEB_HOST" \
     "finish() { \
-        rm -rf \"$DEPLOY_DEB_PACKAGE_DIR/incoming\"; \
+        rm -rf \"$INCOMING_DIR\"; \
      }; \
      \
      trap finish EXIT; \
      \
      cd \"$DEPLOY_DEB_PACKAGE_DIR/repos/$DEPLOY_DEB_REPO\" \
-        && reprepro -V includedeb $DEPLOY_DEB_CODENAME \"$DEPLOY_DEB_PACKAGE_DIR/incoming/\"*.deb"
+        && reprepro -V includedeb $DEPLOY_DEB_CODENAME \"$INCOMING_DIR/\"*.deb"
